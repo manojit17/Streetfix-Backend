88958904fb07434e52e01ff09674867ec6f7c6d2
manojit17 <manojit75431@gmail.com>
2026-06-19 18:22:10 +0530

    change in server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();   // ← MUST come before app.use()
+
 const corsOptions = {
   origin: [
     "http://localhost:5173",
